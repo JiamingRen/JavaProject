dee37a7c6d41a15343fda445416255fff0702246
TIM REN <renjiam@deakin.edu.au>
2022-04-10 18:24:36 +1000

    update

--- BundleCalculator/Src/BundleCalculator.java
+++ BundleCalculator/Src/BundleCalculator.java
@@ -1,5 +1,8 @@
 package Src;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -41,7 +44,6 @@ public class BundleCalculator {
 
     public void getTotalPrice(String formatCode, HashMap<String, Integer> orderMap) {
 
-
         formatCode = formatCode.toLowerCase();
 
         double formatAmount = orderMap.get(formatCode);
@@ -52,41 +54,62 @@ public class BundleCalculator {
         Collections.reverse(bundlePrice);
         List<Double> bundleSize = getBundleSize(format);
         Collections.reverse(bundleSize);
-        for (int i = 0; i < bundleSize.size(); i++) {
-            if (i + 1 < bundleSize.size()) {
-                double bundle = (double) (formatAmount / (Double) bundleSize.get(i));
-                // System.out.println("bundle: " + i + " :" + bundle);
-                totalPrice += (int) bundle * (Double) bundlePrice.get(i);
-                formatAmount = (Double) ((bundle - (int) bundle) * (Double) bundleSize.get(i));
-                // System.out.println("formatAmount: " + i + " :" + formatAmount);
 
+        try {
+            FileWriter fWriter = new FileWriter("bill.txt",true);
+            fWriter.write(formatCode + "\n");
+            for (int i = 0; i < bundleSize.size(); i++) {
+                if (i + 1 < bundleSize.size()) {
+                    double bundle = (double) (formatAmount / (Double) bundleSize.get(i));
+                    // System.out.println("bundle: " + i + " :" + bundle);
+                    totalPrice += (int) bundle * (Double) bundlePrice.get(i);
+                    formatAmount = (Double) ((bundle - (int) bundle) * (Double) bundleSize.get(i));
+                    // System.out.println("formatAmount: " + i + " :" + formatAmount);
+                    fWriter.write("BundleSize: "+ (Double) bundleSize.get(i) + " have " + (int) bundle + " bundles" + "\n");
+                    fWriter.write("Pirce Calculation: \n");
+                    fWriter.write((int) bundle + " x " + (Double) bundlePrice.get(i) + " = " + (int) bundle * (Double) bundlePrice.get(i) + "\n");
+                }
+                if (i + 1 == bundleSize.size()) {
+                    double bundle = (formatAmount / (double) bundleSize.get(i));
+                    // System.out.println("bundle: " + i + " :" + bundle);
+                    totalPrice += (int) bundle * (double) bundlePrice.get(i);
+                    formatAmount = (bundle - (int) bundle) * (double) bundleSize.get(i);
+                    // System.out.println("formatAmount: " + i + " :" + formatAmount);
+                }
+                if (formatAmount < (Double) bundleSize.get(bundleSize.size() - 1)) {
+                    totalPrice += formatAmount * (Double) bundlePrice.get(bundlePrice.size() - 1)
+                            / (Double) bundleSize.get(bundleSize.size() - 1);
+                }
             }
-            if (i + 1 == bundleSize.size()) {
-                double bundle = (formatAmount / (double) bundleSize.get(i));
-                // System.out.println("bundle: " + i + " :" + bundle);
-                totalPrice += (int) bundle * (double) bundlePrice.get(i);
-                formatAmount = (bundle - (int) bundle) * (double) bundleSize.get(i);
-                // System.out.println("formatAmount: " + i + " :" + formatAmount);
-            }
-            if (formatAmount < (Double) bundleSize.get(bundleSize.size() - 1)) {
-                totalPrice += formatAmount * (Double) bundlePrice.get(bundlePrice.size() - 1)
-                        / (Double) bundleSize.get(bundleSize.size() - 1);
-            }
+            fWriter.write("\n");
+            fWriter.close();
+        } catch (IOException e) {
+            // Print the exception
+            System.out.print(e.getMessage());
         }
     }
 
     public static void main(String[] args) {
-        
-        BundleCalculator BundleCalculator = new BundleCalculator();
 
+        BundleCalculator BundleCalculator = new BundleCalculator();
         Order order = new Order();
         // get customer order
         List<String> readOrder = order.readOrder();
         // save customer order in a map
         HashMap<String, Integer> orderMap = BundleCalculator.getOrder(readOrder);
-        for (String i : orderMap.keySet()) {
-            BundleCalculator.getTotalPrice(i,orderMap);
 
+        for (String i : orderMap.keySet()) {
+            BundleCalculator.getTotalPrice(i, orderMap);
+
+        }
+        try {
+            FileWriter fWriter = new FileWriter("bill.txt", true);
+            fWriter.write("Total Price: " + BundleCalculator.totalPrice + "\n");
+            fWriter.write("\n");
+            fWriter.close();
+        } catch (IOException e) {
+            // Print the exception
+            System.out.print(e.getMessage());
         }
         System.out.println(BundleCalculator.totalPrice);
     }
